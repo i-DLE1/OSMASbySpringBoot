df711667d738ec1ee928a00153ef0e087ecb6157
KimHaedeun <eun06151@naver.com>
2023-06-21 16:56:32 +0900

    first commit

---
+++ src/main/java/com/idle/osmas/OsmasApplication.java
@@ -0,0 +1,13 @@
+package com.idle.osmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsmasApplication.class, args);
+    }
+
+}
