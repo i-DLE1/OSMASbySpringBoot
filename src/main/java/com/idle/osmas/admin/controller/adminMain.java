cd33e7ed48af85858d392363488439fc17b762bf
KimHaedeun <eun06151@naver.com>
2023-06-26 11:52:25 +0900

    매핑 테스트2

---
+++ src/main/java/com/idle/osmas/admin/controller/adminMain.java
@@ -0,0 +1,12 @@
+package com.idle.osmas.admin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+@RequestMapping("/admin")
+public class adminMain {
+
+    @GetMapping("adminMain")
+    public void adminMain () {}
+}
