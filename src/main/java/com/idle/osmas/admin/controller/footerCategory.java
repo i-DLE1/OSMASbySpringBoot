338ca0a530a8bacc339855a73eba50913c4d815a
KimHaedeun <eun06151@naver.com>
2023-06-26 16:34:30 +0900

    페이지 매핑

--- src/main/java/com/idle/osmas/admin/controller/footerCategory.java
+++ src/main/java/com/idle/osmas/admin/controller/footerCategory.java
@@ -1,4 +1,22 @@
 package com.idle.osmas.admin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/footerCategory")
 public class footerCategory {
+
+    @GetMapping("OSAMSIntroduction")
+    public void OSAMSIntroduction(){}
+
+    @GetMapping("useTerms")
+    public void useTerms(){}
+
+    @GetMapping("personalInformationProcessingPolicy")
+    public void personalInformationProcessingPolicy(){}
+
+    @GetMapping("projectJudgingCriteria")
+    public void projectJudgingCriteria(){}
 }
