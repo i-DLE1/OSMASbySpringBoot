da6342b4a1dd7e40e795ecdd19b0b7c1bcb7a878
miji <mjyeo6031@gmail.com>
2023-06-26 11:52:45 +0900

    메인페이지 생성, 메인컨트롤러 생성

---
+++ src/main/java/com/idle/osmas/common/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.idle.osmas.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @GetMapping(value = {"/", "/main"})
+    public String main(){
+        return "main/main";
+    }
+}
