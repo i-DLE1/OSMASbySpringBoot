af216e40d4ef1d0e35327b4ec0b9d4521f3e990d
miji <mjyeo6031@gmail.com>
2023-06-26 12:35:28 +0900

    판매페이지 수정

--- src/main/java/com/idle/osmas/seller/controller/SalesPageController.java
+++ src/main/java/com/idle/osmas/seller/controller/SalesPageController.java
@@ -9,5 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SalesPageController {
     @GetMapping("salesPage1")
     public void salesPage1(){}
+    @GetMapping("salesPage2")
+    public void salesPage2(){}
+    @GetMapping("salesPage3")
+    public void salesPage3(){}
+    @GetMapping("salesPage4")
+    public void salesPage4(){}
+    @GetMapping("salesPage5")
+    public void salesPage5(){}
+    @GetMapping("salesPage6")
+    public void salesPage6(){}
 
 }
