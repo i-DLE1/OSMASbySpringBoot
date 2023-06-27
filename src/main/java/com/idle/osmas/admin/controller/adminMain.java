8f0e2e339df571ab176678dc4d20012e2e66d191
KimHaedeun <eun06151@naver.com>
2023-06-27 18:03:55 +0900

    판매자 신청 관리 SQL 작성

--- src/main/java/com/idle/osmas/admin/controller/adminMain.java
+++ src/main/java/com/idle/osmas/admin/controller/adminMain.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
-//@RequestMapping("/admin")
+@RequestMapping("/admin")
 public class adminMain {
 
     @GetMapping(value = {"/", "admin"})
