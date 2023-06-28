4f1d8a08777681f221c4246ef320e0867a850c6d
yuju1025 <zzgmlo3o@naver.com>
2023-06-28 15:01:53 +0900

    commit

---
+++ src/main/java/com/idle/osmas/admin/controller/admin_noticeController.java
@@ -0,0 +1,22 @@
+package com.idle.osmas.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin/admin_notice")
+public class admin_noticeController {
+
+    @GetMapping("admin_article")
+    public void admin_article(){}
+
+    @GetMapping("admin_event")
+    public void admin_event(){}
+
+    @GetMapping("notice_management")
+    public void notice_management(){}
+
+    @GetMapping("notice_registration")
+    public void notice_registration(){}
+}
