bfd7d0eb00e7b9a452b967c06634da9aa6423609
KimHaedeun <eun06151@naver.com>
2023-06-28 10:29:08 +0900

    버튼 수정

--- src/main/resources/static/js/admin/seller&itemApproval/waitingAuthority.js
+++ src/main/resources/static/js/admin/seller&itemApproval/waitingAuthority.js
@@ -14,7 +14,6 @@ document.addEventListener('DOMContentLoaded', function() {
         button.addEventListener('click', () => {
             const product = button.closest('.product');
             const sellerName = product.querySelector('.seller-name').textContent;
-            const saleDescription = product.querySelector('.sale-description').textContent;
 
             // 알림 폼 생성
             const notificationForm = document.createElement('div');
@@ -22,13 +21,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
             // 폼 내용 설정
             notificationForm.innerHTML = `
-      <h4>부여 보류 알림</h4>
-      <p>판매자: ${sellerName}</p>
-      <p>판매 내용: ${saleDescription}</p>
-    `;
+        <h4>부여 보류 알림</h4>
+        <p>판매자: ${sellerName}</p>
+        <div class="form-content">
+          <input class="reasonText" type="text" placeholder="내용을 입력하세요">
+          <div class="button-container">
+            <button class="send-button">전송</button>
+            <button class="close-button">닫기</button>
+          </div>
+        </div>
+      `;
 
             // 알림 폼을 페이지에 추가
             document.body.appendChild(notificationForm);
+
+            const sendButton = notificationForm.querySelector('.send-button');
+            const closeButton = notificationForm.querySelector('.close-button');
+
+            sendButton.addEventListener('click', () => {
+                const reasonText = notificationForm.querySelector('.reasonText').value;
+                // 전송 처리
+                console.log('전송:', reasonText);
+            });
+
+            closeButton.addEventListener('click', () => {
+                // 알림 폼 닫기
+                notificationForm.remove();
+            });
         });
     });
 });
