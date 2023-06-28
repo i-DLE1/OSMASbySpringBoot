5c979b494c89f09578831f97e53ab327e1c8c704
KimHaedeun <eun06151@naver.com>
2023-06-28 11:04:19 +0900

    버튼 알림창 띄우기

--- src/main/resources/static/js/admin/seller&itemApproval/waitingRetrieve.js
+++ src/main/resources/static/js/admin/seller&itemApproval/waitingRetrieve.js
@@ -1,11 +1,53 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    const buyButtons = document.querySelectorAll('.buy-button');
+    const goButtons = document.querySelectorAll('.goButton');
+    const noButtons = document.querySelectorAll('.noButton');
 
-    buyButtons.forEach(button => {
+    goButtons.forEach(button => {
         button.addEventListener('click', () => {
             // 알림 메시지 표시
-            alert('권한 회수가 완료되었습니다!');
+            alert('권한이 회수되었습니다!');
+        });
+    });
+
+    noButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const product = button.closest('.product');
+            const sellerName = product.querySelector('.seller-name').textContent;
+
+            // 알림 폼 생성
+            const notificationForm = document.createElement('div');
+            notificationForm.classList.add('notification-form');
+
+            // 폼 내용 설정
+            notificationForm.innerHTML = `
+        <h4>회수 보류 알림</h4>
+        <p>판매자: ${sellerName}</p>
+        <div class="form-content">
+          <input class="reasonText" type="text" placeholder="내용을 입력하세요">
+          <div class="button-container">
+            <button class="send-button">전송</button>
+            <button class="close-button">닫기</button>
+          </div>
+        </div>
+      `;
+
+            // 알림 폼을 페이지에 추가
+            document.body.appendChild(notificationForm);
+
+            const sendButton = notificationForm.querySelector('.send-button');
+            const closeButton = notificationForm.querySelector('.close-button');
+
+            sendButton.addEventListener('click', () => {
+                const reasonText = notificationForm.querySelector('.reasonText').value;
+                // 전송 처리
+                console.log('전송:', reasonText);
+            });
+
+            closeButton.addEventListener('click', () => {
+                // 알림 폼 닫기
+                notificationForm.remove();
+            });
         });
     });
 });
