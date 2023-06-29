0c59077596efd4edcbd7770d2f23314caebee1d0
yuju1025 <zzgmlo3o@naver.com>
2023-06-29 15:18:07 +0900

    commit

--- src/main/java/com/idle/osmas/admin/dto/AdminBoardFileDTO.java
+++ src/main/java/com/idle/osmas/admin/dto/AdminBoardFileDTO.java
@@ -1,5 +1,6 @@
 package com.idle.osmas.admin.dto;
 
+import com.idle.osmas.member.dto.MemberDTO;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -23,4 +24,22 @@ public class AdminBoardFileDTO {
     private char deteleYN; //삭제여부
 
     private AdminBoardDTO adminBoardNo; //게시판 번호
+
+    //추가
+    private int boardNo; // 게시판 번호
+
+    private String boardTitle; // 게시판 제목
+
+    private String boardContent; // 게시판 내용
+
+    private LocalDate boardRegistDate; // 게시판 등록일
+
+    private LocalDate boardModifyDate; // 게시판 수정일
+
+    private char boardDeleteYN; // 게시판 삭제 여부
+
+    private AdminBoardDivisionCode boardClassifyCode; // 게시판 구분 코드
+
+    private MemberDTO boardMemberNo; // 게시판 작성자
+
 }
