be6e60665c68716a6e505d8eb2d5ca4f24b235a7
KimHaedeun <eun06151@naver.com>
2023-07-03 18:18:19 +0900

    제안 변수 받기

--- src/main/java/com/idle/osmas/admin/controller/AdminCategoryController.java
+++ src/main/java/com/idle/osmas/admin/controller/AdminCategoryController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -33,4 +35,18 @@ public class AdminCategoryController {
 
         return "/admin/adminCategory/gettingProposals";
     }
+
+//    @PostMapping("sendingProposals")
+//    public String sendingProposals(@RequestParam("proposalNo") String proposalNo, @RequestParam("reasonText") String reasonText, Model model) {
+//        model.addAttribute("proposalNo", proposalNo);
+//        model.addAttribute("reasonText", reasonText);
+//
+//        int result = GettingProposalsService.sendProposals(proposalNo, reasonText);
+//
+//        if (result > 0) {
+//            return "redirect:/admin/adminCategory/gettingProposals";
+//        } else {
+//            return "redirect:/admin/errorPage";
+//        }
+//    }
 }
