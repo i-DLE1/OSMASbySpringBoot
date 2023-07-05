2b57231d2c3d57b3c7918798f067bedb470b77ad
KimHaedeun <eun06151@naver.com>
2023-07-05 12:48:12 +0900

    파일 받는중

--- src/main/java/com/idle/osmas/admin/controller/SellerApprovalFormController.java
+++ src/main/java/com/idle/osmas/admin/controller/SellerApprovalFormController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.Map;
 
 @Controller
@@ -27,6 +29,28 @@ public class SellerApprovalFormController {
 
     @GetMapping("getForm")
     public void getForm(){}
+
+    @PostMapping("FileUpload")
+    public void FileUpload(@RequestParam("file") MultipartFile file) {
+        String uploadDirectory = "images/admin/getForm";
+
+        if (!file.isEmpty()) {
+            try {
+                // 파일 저장 경로 설정
+                String filePath = uploadDirectory + "/" + file.getOriginalFilename();
+
+                // 파일을 지정된 경로에 저장
+                file.transferTo(new File(filePath));
+
+                // 파일 업로드 성공 처리
+                System.out.println("파일 업로드 성공");
+            } catch (Exception e) {
+                // 파일 업로드 실패 처리
+                System.out.println("파일 업로드 실패");
+                e.printStackTrace();
+            }
+        }
+    }
 
     @GetMapping("outForm")
     public void outForm(){}
@@ -66,6 +90,47 @@ public class SellerApprovalFormController {
             return "redirect:/admin/errorPage";
         }
     }
+
+//    @PostMapping("sellerInsert")
+//    public String sellerInsert(@RequestParam Map<String, String> requestParams, Model model) {
+//        String sellerId = requestParams.get("sellerId");
+//        String bank = requestParams.get("bank");
+//        String accountNo = requestParams.get("accountNo");
+//        String name = requestParams.get("name");
+//        String rprsn = requestParams.get("rprsn");
+//        String callNumber = requestParams.get("callNumber");
+//        String address = requestParams.get("address");
+//        String registNo = requestParams.get("registNo");
+//        String reportNo = requestParams.get("reportNo");
+//
+//        String registFile = requestParams.get("registFile");
+//        String reportFile = requestParams.get("reportFile");
+//        String certificateFile = requestParams.get("certificateFile");
+//        String bankBookFile = requestParams.get("bankBookFile");
+//
+//        model.addAttribute("sellerId", sellerId);
+//        model.addAttribute("bank", bank);
+//        model.addAttribute("accountNo", accountNo);
+//        model.addAttribute("name", name);
+//        model.addAttribute("rprsn", rprsn);
+//        model.addAttribute("callNumber", callNumber);
+//        model.addAttribute("address", address);
+//        model.addAttribute("registNo", registNo);
+//        model.addAttribute("reportNo", reportNo);
+//
+//        model.addAttribute("registFile", registFile);
+//        model.addAttribute("reportFile", reportFile);
+//        model.addAttribute("certificateFile", certificateFile);
+//        model.addAttribute("bankBookFile", bankBookFile);
+//
+//        int result = sellerApprovalFormService.sellerInsert(requestParams);
+//
+//        if (result > 0) {
+//            return "redirect:/admin/sellerApprovalForm/getForm";
+//        } else {
+//            return "redirect:/admin/errorPage";
+//        }
+//    }
 
 
 }
--- src/main/resources/static/js/admin/sellerApprovalForm/getForm.js
+++ src/main/resources/static/js/admin/sellerApprovalForm/getForm.js
@@ -1,14 +1,157 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const submitButtons = document.querySelectorAll('.submit-container input[type="submit"]');
+    var changeButtons = document.querySelectorAll('.submit-container #change');
+    var goButtons = document.querySelectorAll('.submit-container #go');
+    var noButtons = document.querySelectorAll('.submit-container #no');
 
-    submitButtons.forEach(button => {
+    goButtons.forEach(button => {
         button.addEventListener('click', (event) => {
-            event.preventDefault(); // 기본 동작인 폼 제출을 막기 --> 나중에 지워줘야함
+            const action = button.value;
+
+            // 알림창 표시
+            alert(`${action} 되었습니다!`);
+
+            var sellerId = document.getElementById("sellerId").value;
+            var bank = document.getElementById("bank").value;
+            var accountNo = document.getElementById("accountNo").value;
+            var name = document.getElementById("name").value;
+            var rprsn = document.getElementById("rprsn").value;
+            var callNumber = document.getElementById("callNumber").value;
+            var address = document.getElementById("address").value;
+            var registNo = document.getElementById("registNo").value;
+            var reportNo = document.getElementById("reportNo").value;
+
+            var registFile = document.getElementById("registFile").files[0];
+            var reportFile = document.getElementById("reportFile").files[0];
+            var certificateFile = document.getElementById("certificateFile").files[0];
+            var bankBookFile = document.getElementById("bankBookFile").files[0];
+
+            console.log(sellerId);
+            console.log(bank);
+            console.log(accountNo);
+            console.log(name);
+            console.log(rprsn);
+            console.log(callNumber);
+            console.log(address);
+            console.log(registNo);
+            console.log(reportNo);
+
+            console.log(registFile);
+            console.log(reportFile);
+            console.log(certificateFile);
+            console.log(bankBookFile);
+
+
+            var formData = new FormData();
+            formData.append('sellerId', sellerId);
+            formData.append('bank', bank);
+            formData.append('accountNo', accountNo);
+            formData.append('name', name);
+            formData.append('rprsn', rprsn);
+            formData.append('callNumber', callNumber);
+            formData.append('address', address);
+            formData.append('registNo', registNo);
+            formData.append('reportNo', reportNo);
+            formData.append('registFile', registFile);
+            formData.append('reportFile', reportFile);
+            formData.append('certificateFile', certificateFile);
+            formData.append('bankBookFile', bankBookFile);
+
+            // AJAX 요청
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', '/admin/sellerApprovalForm/FileUpload', true);
+            xhr.onload = function() {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    console.log('파일 업로드 성공');
+                    alert('파일 업로드가 완료되었습니다!');
+                } else {
+                    console.error('파일 업로드 실패');
+                }
+                console.log(xhr.responseText);
+            };
+            xhr.send(formData);
+
+            // AJAX 요청
+            $.ajax({
+                url: "/admin/sellerApprovalForm/sellerInsert",
+                method: 'POST',
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function(response) {
+                    console.log('권한 회수 신청이 완료되었습니다.');
+                    alert('권한 회수 신청이 완료되었습니다!');
+                    location.reload(); // 페이지 새로 고침
+                },
+                error: function(error) {
+                    console.error("권한 회수 신청에 실패했습니다.", error);
+                }
+            });
+        });
+    });
+
+    noButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            event.preventDefault(); // 기본 동작 방지
 
             const action = button.value;
 
             // 알림창 표시
             alert(`${action} 되었습니다!`);
+
+            // 판매자 ID 가져오기
+            var sellerName = document.getElementById("sellerName").value;
+
+            console.log(sellerName);
+
+            // AJAX 요청
+            $.ajax({
+                url: "/admin/sellerApprovalForm/sellerInsertCancel",
+                method: 'POST',
+                data: {
+                    'sellerId': sellerName
+                },
+                success: function(response) {
+                    console.log('신청이 취소되었습니다.');
+                    alert('신청이 취소되었습니다!');
+                    location.reload(); // 페이지 새로 고침
+                },
+                error: function(error) {
+                    console.error("신청 취소에 실패했습니다.", error);
+                }
+            });
+        });
+    });
+
+    changeButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            event.preventDefault(); // 기본 동작 방지
+
+            const action = button.value;
+
+            // 알림창 표시
+            alert(`${action} 되었습니다!`);
+
+            // 판매자 ID 가져오기
+            var sellerName = document.getElementById("sellerName").value;
+
+            console.log(sellerName);
+
+            // AJAX 요청
+            $.ajax({
+                url: "/admin/sellerApprovalForm/sellerUpdate",
+                method: 'POST',
+                data: {
+                    'sellerId': sellerName
+                },
+                success: function(response) {
+                    console.log('신청이 취소되었습니다.');
+                    alert('신청이 취소되었습니다!');
+                    location.reload(); // 페이지 새로 고침
+                },
+                error: function(error) {
+                    console.error("신청 취소에 실패했습니다.", error);
+                }
+            });
         });
     });
 });
