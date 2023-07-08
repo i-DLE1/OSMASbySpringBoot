e3a154c01d452e34f9e009a306c254bd963a0bde
heo <gjdbdlfsla147@gmail.com>
2023-07-08 17:19:54 +0900

    회원가입페이지 수정

--- src/main/java/com/idle/osmas/member/controller/SignupCheckController.java
+++ src/main/java/com/idle/osmas/member/controller/SignupCheckController.java
@@ -1,5 +1,6 @@
 package com.idle.osmas.member.controller;
 
+import com.idle.osmas.common.exception.SignUpException;
 import com.idle.osmas.member.service.EmailServiceImpl;
 import com.idle.osmas.member.service.MemberServiceImpl;
 import org.springframework.stereotype.Controller;
@@ -22,45 +23,46 @@ public class SignupCheckController {
     }
     @PostMapping("/idDupCheck")
     @ResponseBody
-    public String checkDUplication(@RequestParam("id") String id){
+    public String checkDUplication(@RequestParam("id") String id) throws SignUpException {
        String result = "사용 가능한 아이디입니다";
         System.out.println(id);
         if("".equals(id)){
-            result = "아이디를 입력 해주세요.";
+            throw new SignUpException("아이디를 입력해주세요");
         } else if(memberService.selectMemberById(id)){
-            result = "중복된 아이디가 있습니다";
+            throw new SignUpException("중복된 아이디가 있습니다");
         }
         return result;
     }
     @PostMapping("/nickDupCheck")
     @ResponseBody
-    public String checkNickDUplication(@RequestParam("nickname") String nickname){
+    public String checkNickDUplication(@RequestParam("nickname") String nickname)throws SignUpException{
         String result = "사용 가능한 닉네임입니다";
         System.out.println(nickname);
         if("".equals(nickname)){
-            result = "닉네임를 입력 해주세요.";
+            throw new SignUpException("닉네임를 입력 해주세요.");
         } else if(memberService.selectMemberByNickname(nickname)){
-            result = "중복된 닉네임 있습니다";
+            throw new SignUpException("중복된 닉네임이 있습니다");
         }
         return result;
     }
     @PostMapping("/emailDupCheck")
     @ResponseBody
-    public String checkEmailDUplication(@RequestParam("email") String email){
+    public String checkEmailDUplication(@RequestParam("email") String email)throws SignUpException{
         String result = "사용 가능한 이메일입니다";
         System.out.println(email);
         if("".equals(email)){
-            result = "이메일을 입력 해주세요.";
+            throw new SignUpException("이메일을 입력 해주세요.");
         } else if(memberService.selectMemberByEmail(email)){
-            result = "중복된 이메일이 있습니다";
+            throw new SignUpException("중복된 이메일이 있습니다");
         }
         return result;
     }
-
     @PostMapping("/email")
     @ResponseBody
     public String emailSend(@RequestParam("email") String email) throws MessagingException, UnsupportedEncodingException {
-    String result = emailService.mailSend(email);
-    return result;
+
+        return emailService.mailSend(email);
+
     }
 }
+
--- src/main/resources/static/js/member/signUp.js
+++ src/main/resources/static/js/member/signUp.js
@@ -2,10 +2,10 @@ function eamilOk(){
     const emailNum = $('#emailNum').val();
     const emailCheck = $('#emailCheck').val();
     if(emailNum == emailCheck){
-        alert('인증에 성공했습니다')
+        $('#emailNum-error').text('인증에 성공했습니다');
         $('#signupBtn').removeAttr('disabled')
     }else{
-        alert('인증에 실패했습니다')
+        $('#emailNum-error').text('인증에 실패했습니다');
         $('#emailNum').val('');
         $('#emailNum').focus();
     }
@@ -15,7 +15,8 @@ function idReg() {
     const id = $("#id").val();
     let idCheck = /^[a-zA-Z0-9]{5,20}$/;
     if (!idCheck.test(id)) {
-        alert("아이디는 영문또는 숫자로 5자~20자로 입력해주세요.")
+        $('#id-error').text("아이디는 영문또는 숫자로 5자~20자로 입력해주세요.");
+        // alert("아이디는 영문또는 숫자로 5자~20자로 입력해주세요.")
         $("#id").val('');
         $("#id").focus();
     } else {
@@ -24,14 +25,12 @@ function idReg() {
             type: "post",
             data: {id: id},
             success: function (data) {
-                if (data == '중복된 아이디가 있습니다') {
-                    alert(data)
-                    $('#id').val('');
-                    $("#id").focus();
-                }
+                $('#id-error').text(data);
             },
             error: function (error) {
-                alert(error)
+                $('#id-error').text(error.responseJSON.message);
+                $('#id').val('');
+                $("#id").focus();
             }
         });
     }
@@ -40,7 +39,7 @@ function emailReg(){
     const email = $('#email').val();
     let emailCheck =  /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
     if(!emailCheck.test(email)){
-        alert('다시 입력해주세요')
+        $('#email-error').text('다시 입력해주세요');
         $('#email').val('');
         $('#email').focus();
     }else{
@@ -49,14 +48,12 @@ function emailReg(){
             type: "post",
             data: {email: email},
             success: function (data) {
-                if (data == '중복된 이메일이 있습니다') {
-                    alert(data)
-                    $('#email').val('');
-                    $("#email").focus();
-                }
+                $('#email-error').text(data);
             },
             error: function (error) {
-                alert(error)
+                $('#email-error').text(error.responseJSON.message);
+                $('#email').val('');
+                $("#email").focus();
             }
         });
     }
@@ -66,7 +63,7 @@ function nickReg() {
     const nickname = $("#nickname").val();
     let nickCheck = /^[가-힣]{2,8}$/;
     if (!nickCheck.test(nickname)) {
-        alert("닉네임은 한글로 2~8자로 입력해주세요.")
+        $('#nickname-error').text("닉네임은 한글로 2~8자로 입력해주세요.");
         $("#nickname").val('');
         $("#nickname").focus();
     }else{
@@ -75,14 +72,12 @@ function nickReg() {
             type: "post",
             data: {nickname: nickname},
             success: function (data) {
-                if (data == '중복된 닉네임 있습니다') {
-                    alert(data)
-                    $('#nickname').val('');
-                    $("#nickname").focus();
-                }
+                $('#nickname-error').text(data);
             },
             error: function (error) {
-                alert(error)
+                $('#nickname-error').text(error.responseJSON.message);
+                $('#nickname').val('');
+                $("#nickname").focus();
             }
         });
     }
@@ -91,7 +86,7 @@ function  pwdReg(){
     const pwd = $('#pwd').val();
     let pwdCheck = /^[0-9a-zA-Z]{6,15}$/;
     if(!pwdCheck.test(pwd)){
-        alert('비밀번호는 영문,숫자 혼합하여 8자~15자로 입력해주세요')
+        $('#pwd-error').text('비밀번호는 영문,숫자 혼합하여 8자~15자로 입력해주세요.');
         $('#pwd').val('');
         $('#pwd').focus();
     }
@@ -100,27 +95,33 @@ function nameReg(){
     const name = $('#name').val();
     let nameCheck = /^[가-힣]{2,4}$/;
     if(!nameCheck.test(name)){
-        alert('이름을 다시 입력해주세요.')
+        $('#name-error').text('이름을 다시 입력해주세요.');
         $('#name').val('');
         $('#name').focus();
+    }else{
+        $('#name-error').text('');
     }
 }
 function phoneReg(){
     const phone = $('#phone').val();
     let phoneCheck = /^\d{2,3}-\d{3,4}-\d{4}$/;
     if(!phoneCheck.test(phone)){
-        alert('전화번호를 다시 입력해주세요.')
+        $('#phone-error').text('전화번호를 다시 입력해주세요.');
         $('#phone').val('');
         $('#phone').focus();
+    }else{
+        $('#phone-error').text('');
     }
 }
 function birthReg(){
     const birth = $('#birth').val();
     let birthCheck = /^(19[0-9][0-9]|20\d{2})(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])$/;
     if(!birthCheck.test(birth)){
-        alert('다시 입력해주세요.')
+        $('#birth-error').text('생년월일은 8자리 숫자로 입력해주세요.');
         $('#birth').val('');
         $('#birth').focus();
+    }else{
+        $('#birth-error').text('');
     }
 }
 
@@ -131,10 +132,10 @@ function emailSend(){
         type: "post",
         data:{email : email},
         success : function (data){
-            alert('이메일에 인증번호를 보냈습니다')
+            $('#email-error').text('이메일에 인증번호를 보냈습니다');
             $('#emailCheck').val(data);
         },error : function (error){
-            alert('이메일을 다시 확인해주세요')
+            $('#email-error').text('이메일을 다시 확인해주세요');
         }
 
     });
