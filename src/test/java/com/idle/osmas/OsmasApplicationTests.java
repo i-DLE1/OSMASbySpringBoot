c6fe1a0218f0428ab775c3e99865cbb87dba805c
KimHaedeun <eun06151@naver.com>
2023-07-10 16:23:30 +0900

    권한 롤에 따른 마이페이지 수정

--- src/test/java/com/idle/osmas/OsmasApplicationTests.java
+++ src/test/java/com/idle/osmas/OsmasApplicationTests.java
@@ -1,13 +1,6 @@
 package com.idle.osmas;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class OsmasApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
