fd636ff5733385f828e4f86ddff0c1a5acabb650
heo <gjdbdlfsla147@gmail.com>
2023-07-10 15:33:11 +0900

    pay

--- src/main/java/com/idle/osmas/member/controller/PayController.java
+++ src/main/java/com/idle/osmas/member/controller/PayController.java
@@ -1,9 +1,6 @@
 package com.idle.osmas.member.controller;
 
-import com.idle.osmas.member.dto.AddressDTO;
-import com.idle.osmas.member.dto.MemberDTO;
-import com.idle.osmas.member.dto.PayDTO;
-import com.idle.osmas.member.dto.ProductsDTO;
+import com.idle.osmas.member.dto.*;
 import com.idle.osmas.member.service.MemberServiceImpl;
 import com.idle.osmas.member.service.PayServiceImpl;
 import org.springframework.stereotype.Controller;
@@ -26,17 +23,39 @@ public class PayController {
         this.payService = payService;
         this.memberService = memberService;
     }
-    @GetMapping("/pay/pay")
-    public void goPay(Model m, Principal principal, @RequestParam("no") int no,@RequestParam("productNo") List<Integer> productNo,@RequestParam("count") List<Integer> count){
+//    @GetMapping("/pay/pay")
+//    public void goPay(Model m, Principal principal, @RequestParam("no") int no,@RequestParam("productNo") List<Integer> productNo,@RequestParam("count") List<Integer> count){
+//        String id = principal.getName();
+//        MemberDTO member = payService.selectMemberById(id);
+//        AddressDTO address = payService.selectAddressByNo(member.getNo());
+//        Long price = 0L;
+//        List<ProductsDTO> product = new ArrayList<>();
+//        ProductsDTO products;
+//        for(int i = 0 ; i < productNo.size();i++){
+//            products = payService.selectProduct(productNo.get(i));
+//            products.setCount(count.get(i));
+//            product.add(products);
+//            price += products.getPrice() * products.getCount();
+//        }
+//        System.out.println(product);
+//        PayDTO pay = payService.selectPay(no);
+//        pay.setSumPrice(price);
+//        m.addAttribute("productList", product);
+//        m.addAttribute("pay",pay);
+//        m.addAttribute("member",member);
+//        m.addAttribute("address",address);
+//    }
+    @PostMapping("/pay/pay")
+    public void goPay(Model m, Principal principal, @RequestParam("no") int no,@RequestParam("productList") List<OptionDTO> productList){
         String id = principal.getName();
         MemberDTO member = payService.selectMemberById(id);
         AddressDTO address = payService.selectAddressByNo(member.getNo());
         Long price = 0L;
         List<ProductsDTO> product = new ArrayList<>();
         ProductsDTO products;
-        for(int i = 0 ; i < productNo.size();i++){
-            products = payService.selectProduct(productNo.get(i));
-            products.setCount(count.get(i));
+        for(int i = 0 ; i < productList.size();i++){
+            products = payService.selectProduct(productList.get(i).getOptionNumber());
+            products.setCount(productList.get(i).getOptionAmount());
             product.add(products);
             price += products.getPrice() * products.getCount();
         }
@@ -48,7 +67,6 @@ public class PayController {
         m.addAttribute("member",member);
         m.addAttribute("address",address);
     }
-
     @PostMapping("/pay/address")
     @ResponseBody
     public String addAddress(Principal principal,@RequestBody AddressDTO address) throws Exception {
@@ -64,4 +82,6 @@ public class PayController {
         address.setRefMemberNo(memberService.selectNoByNickname(memberService.selectNicknameById(id)));
         return payService.modAddress(address);
     }
+
+
 }
