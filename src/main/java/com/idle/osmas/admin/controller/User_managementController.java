31ba92af87edac91e6c3e262d8c5290e20bdd9ed
yuju1025 <zzgmlo3o@naver.com>
2023-07-12 13:19:56 +0900

    회원관리 페이징중

--- src/main/java/com/idle/osmas/admin/controller/User_managementController.java
+++ src/main/java/com/idle/osmas/admin/controller/User_managementController.java
@@ -1,22 +1,47 @@
 package com.idle.osmas.admin.controller;
 
+import com.idle.osmas.admin.dto.UserManagementDTO;
+import com.idle.osmas.admin.service.UserManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.List;
 
 @Controller
 @RequestMapping("admin/user_management")
 public class User_managementController {
 
-    @GetMapping("full_inquiry")
-    public void full_inquiry(){}
+    private final UserManagementService userManagementService;
 
-    @GetMapping("seller_inquiry")
-    public void seller_inquiry(){}
+    @Autowired
+    public User_managementController(UserManagementService userManagementService) {
+        this.userManagementService = userManagementService;
+    }
+
+//    회원 전체 조회
+    @GetMapping("/full_inquiry")
+    public String full_inquiry(Model model) {
+        List<UserManagementDTO> users = userManagementService.getAllUsers();
+        model.addAttribute("users", users);
+        return "admin/user_management/full_inquiry";
+    }
+
+    @GetMapping("/seller_inquiry")
+    public void seller_inquiry() {
+        // Seller inquiry logic
+    }
 
     @GetMapping("sponsor_inquiry")
-    public void sponsor_inquiry(){}
+    public void sponsor_inquiry() {
+        // Sponsor inquiry logic
+    }
 
     @GetMapping("withdraw_inquiry")
-    public void withdraw_inquiry(){}
+    public void withdraw_inquiry() {
+        // Withdraw inquiry logic
+    }
 }
